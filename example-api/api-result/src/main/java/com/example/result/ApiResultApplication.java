fcd2856d1c1f950cfa197f549079c8369cb4e3a1
tn <1445763190@qq.com>
2020-12-18 14:16:17 +0800

    add example  apis-result

--- example-api/api-result/src/main/java/com/example/result/ApiResultApplication.java
+++ example-api/api-result/src/main/java/com/example/result/ApiResultApplication.java
@@ -1,13 +1,40 @@
 package com.example.result;
 
+import com.detabes.result.page.ResourcePage;
+import com.detabes.result.result.ResultVO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller("/")
 public class ApiResultApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiResultApplication.class, args);
     }
 
+
+    /**
+     * 普通返回
+     * @return
+     */
+    @GetMapping("/testResultVO")
+    @ResponseBody
+    public ResultVO<String> testResultVO(){
+        return ResultVO.success("hi result");
+    }
+
+    /**
+     * 分页返回
+     * @return
+     */
+    @GetMapping("/testResultVO_Page")
+    @ResponseBody
+    public ResultVO<String> testResultVO_Page(){
+        ResourcePage page = new ResourcePage();
+        return ResultVO.success("hi result",page);
+    }
 }
