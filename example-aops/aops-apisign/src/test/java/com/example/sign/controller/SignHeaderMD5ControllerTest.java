e11b6b2e5d46ffb7119021a61edd02901f40e18b
en-o <13320385140@163.com>
2020-12-22 09:35:48 +0800

    SignHeaderMD5ControllerTest 单元测试新增错误实例

--- example-aops/aops-apisign/src/test/java/com/example/sign/controller/SignHeaderMD5ControllerTest.java
+++ example-aops/aops-apisign/src/test/java/com/example/sign/controller/SignHeaderMD5ControllerTest.java
@@ -13,9 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
@@ -61,6 +63,28 @@ class SignHeaderMD5ControllerTest {
                 .andExpect(jsonPath("$.success", is(true)))
                 .andReturn();
 
+        // - 模拟数据中途被篡改
+        // 参数顺序错误  加密顺序 age,name  参数顺序name,age,
+        // 在使用bean json序列化是顺序的差异会被掩盖导致顺序一致判断失效
+        UserEntity userParamsSortError = UserEntity.builder().name("谭宁").age("123").build();
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/header/isSignMD5BeanPJsonH")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .headers(heanders)
+                .content(JSONObject.toJSONString(userParamsSortError)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", is(true)))
+                .andReturn();
+
+        // 数据更改
+        UserEntity userParamsUpdateError = UserEntity.builder().name("谭wq宁").age("12w3").build();
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/header/isSignMD5BeanPJsonH")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .headers(heanders)
+                .content(JSONObject.toJSONString(userParamsUpdateError)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect((ResultMatcher) jsonPath("$.message", is("签名不正确")))
+                .andReturn();
     }
 
     /**
@@ -80,6 +104,27 @@ class SignHeaderMD5ControllerTest {
                 .headers(heanders))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success", is(true)))
+                .andReturn();
+
+        // - 模拟数据中途被篡改
+        // 参数顺序错误  加密顺序 age,name  参数顺序name,age ()，get方式强制严重参数顺序
+        String userParamsSortError = String.format("name=%s&age=%s", user.getName(), user.getAge());
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/header/isSignMD5BeanH?"+userParamsSortError)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .headers(heanders))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect((ResultMatcher) jsonPath("$.message", is("签名不正确")))
+                .andReturn();
+
+        // 数据更改
+        String userParamsUpdateError = String.format("age=%s&name=%s","tan","raa");
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/header/isSignMD5BeanH?"+userParamsUpdateError)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .headers(heanders))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect((ResultMatcher) jsonPath("$.message", is("签名不正确")))
                 .andReturn();
 
     }
@@ -111,6 +156,35 @@ class SignHeaderMD5ControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success", is(true)))
                 .andReturn();
+
+        // - 模拟数据中途被篡改
+        // 参数顺序错误
+        ArrayList<UserEntity> sortUser = new ArrayList<>(userArrayList);
+        Collections.reverse(sortUser);
+        String userParamsSortError = JSONArray.toJSONString(sortUser);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/header/isSignMD5ListBeanPH")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .headers(heanders)
+                .content(userParamsSortError))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect((ResultMatcher) jsonPath("$.message", is("签名不正确")))
+                .andReturn();
+
+        // 数据更改
+        user = UserEntity.builder().age("789").name("谭宁3").build();
+        userArrayList.add(user);
+        // 获取加密串
+        String userParamsUpdateError = JSONArray.toJSONString(userArrayList);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/header/isSignMD5ListBeanPH")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .headers(heanders)
+                .content(userParamsUpdateError))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect((ResultMatcher) jsonPath("$.message", is("签名不正确")))
+                .andReturn();
+
     }
 
 
