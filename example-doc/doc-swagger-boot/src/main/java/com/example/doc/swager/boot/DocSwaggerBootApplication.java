8630319d8271ad0b381be444471951f7f50365d7
en-o <13320385140@163.com>
2020-12-22 15:32:08 +0800

    doc.swager.boot

---
+++ example-doc/doc-swagger-boot/src/main/java/com/example/doc/swager/boot/DocSwaggerBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.doc.swager.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocSwaggerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocSwaggerBootApplication.class, args);
+	}
+
+}
---
+++ example-doc/doc-swagger-boot/src/test/java/com/example/doc/swager/boot/DocSwaggerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doc.swager.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocSwaggerBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
