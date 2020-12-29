69e40481e2163178e9a3b8d2d8498500a8ce8b20
en-o <13320385140@163.com>
2020-12-29 16:25:41 +0800

    doc-swagger 集成nacos

--- example-doc/doc-swagger-cloud/src/main/java/com/example/doc/swagger/cloud/DocSwaggerCloudApplication.java
+++ example-doc/doc-swagger-cloud/src/main/java/com/example/doc/swagger/cloud/DocSwaggerCloudApplication.java
@@ -2,8 +2,13 @@ package com.example.doc.swagger.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author tnnn
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DocSwaggerCloudApplication {
 
 	public static void main(String[] args) {
