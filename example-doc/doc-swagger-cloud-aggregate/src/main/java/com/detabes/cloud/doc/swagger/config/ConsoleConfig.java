4de1856720c10f870292f260293a09592b37fe11
tn <1445763190@qq.com>
2021-01-06 14:09:15 +0800

    优化 依赖

--- example-doc/doc-swagger-cloud-aggregate/src/main/java/com/detabes/cloud/doc/swagger/config/ConsoleConfig.java
+++ example-doc/doc-swagger-cloud-aggregate/src/main/java/com/detabes/cloud/doc/swagger/config/ConsoleConfig.java
@@ -1,7 +1,7 @@
 package com.detabes.cloud.doc.swagger.config;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
