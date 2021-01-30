b108a5aae9c6f430e576eaae2abf089838a0b58d
en-o <13320385140@163.com>
2021-01-30 22:30:15 +0800

    接口版本

---
+++ example-aops/aops-apiversion/src/main/java/io/tn/aop/version/AopsApiversionApplication.java
@@ -0,0 +1,12 @@
+package io.tn.aop.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopsApiversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopsApiversionApplication.class, args);
+	}
+}
